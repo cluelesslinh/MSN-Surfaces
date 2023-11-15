a86db55d685d952fbf8828c7e2ebce2107982f8a
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2023-11-15 20:07:18 +0700

    home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
             <section id="Sangsite">
                 <Parallax className="homeTopBg" strength={700}>
                     <Background>
-                        <img className="homeTopImg" src="./public/home-main.jpg" alt="home-main" />
+                        <img className="homeTopImg" src="https://images.unsplash.com/photo-1507089947368-19c1da9775ae?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="home-main" />
                     </Background>
                     <TopTitleBox>
                         <TopTitle>Sangsite</TopTitle>

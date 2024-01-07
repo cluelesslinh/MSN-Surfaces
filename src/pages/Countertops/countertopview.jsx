b2463548bcb0d8bc2318eccf9212c910b41aa714
Linh Pham <153252933+sangsite@users.noreply.github.com>
2024-01-07 22:54:23 +0700

    countertopview
    working on layout

--- src/pages/Countertops/countertopview.jsx
+++ src/pages/Countertops/countertopview.jsx
@@ -1,30 +1,39 @@
 /* eslint-disable react/prop-types */
+import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import "./countertopview.scss";
+
 export const CountertopView = ({ countertop, onBackClick }) => {
     return (
         <>
-            <div>
-                <img className="img-fluid" src={countertop.image} />
-            </div>
-            <div>
-                <span>Description: </span>
-                <span>{countertop.description}</span>
-            </div>
-            <div>
-                <span>{countertop.title}</span>
-            </div>
-            <div>
-                <span>Primary Color: </span>
-                <span>{countertop.primarycolor}</span>
-            </div>
-            <div>
-                <span>Accent Color: </span>
-                <span>{countertop.accentcolor}</span>
-            </div>
-            <div>
-                <span>Material: </span>
-                <span>{countertop.material}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
+
+            <MDBRow className="countertopBox">
+                <MDBCol className="countertopViewTitle col-12">
+                    <span>{countertop.title}</span>
+                </MDBCol>
+                <MDBCol className="countertopViewRow1 col-5">
+                    <img className="img-fluid" src={countertop.image} />
+                </MDBCol>
+                <MDBCol className="countertopViewRow1 col-5">
+                    <span>Description: </span>
+                    <span>{countertop.description}</span>
+                </MDBCol>
+                <MDBCol className="col-3 offset-1">
+                    <span>Primary Color: </span>
+                    <span>{countertop.primarycolor}</span>
+                </MDBCol>
+                <MDBCol className="col-3">
+                    <span>Accent Color: </span>
+                    <span>{countertop.accentcolor}</span>
+                </MDBCol>
+                <MDBCol className="col-3">
+                    <span>Material: </span>
+                    <span>{countertop.material}</span>
+                </MDBCol>
+                <MDBCol className="backButton col-12">
+                    <button onClick={onBackClick}>Back</button>
+                </MDBCol>
+            </MDBRow>
+
         </>
     );
 };

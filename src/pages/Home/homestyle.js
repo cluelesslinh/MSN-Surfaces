0f9f5a06e1f984bec92ee200f0fea133216f8986
Linh Pham <153252933+sangsite@users.noreply.github.com>
2024-02-16 13:43:35 +0700

    home.jsx
    fixed margins for mobile view

--- src/pages/Home/homestyle.js
+++ src/pages/Home/homestyle.js
@@ -7,10 +7,10 @@ width: 425px;
 display: flex;
 align-items: center;
 justify-content: center;
-margin: 5rem 0 2rem 60%;
+margin: 5em 0 2em 75%;
 flex-wrap: wrap;
 @media screen and (max-width: 768px) {
-    margin: 3% 0 3% 3%;
+    margin: 3em 0 0 0;
 }
 `;
 
@@ -27,11 +27,10 @@ background-color: white;
 width: 425px;
 max-width: 625px;
 min-width: 425px;
-margin: 0 0 0 60%;
+margin: 0 0 0 75%;
 padding: 1em 0 1em 0;
 @media screen and (max-width: 768px) {
-    min-width: 425px;
-    margin: 3% 0 3% 3%;
+    margin: 3em 0 0 0;
 }
 `;
 
